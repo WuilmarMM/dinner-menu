204134467f961d7d47ae2da38f047f3424ccd62e
WuilmarMM <77976613+WuilmarMM@users.noreply.github.com>
2021-03-08 16:35:51 -0500

    March 8, 2021 4:35 PM

--- src/components/MenuItem/index.js
+++ src/components/MenuItem/index.js
@@ -1,14 +1,14 @@
 import React from "react"
 
 export default class MenuItem extends React.Component {
-  state = {
-    numbers:[0]
+  numberArr = {
+    value:[0]
   }
   
   handleClick() {
-    this.setState();{
-      numbers.push(number+1)
-    }
+    this.setState(prevState => ({
+      numberArr: [...prevState.numberArr, this.value]
+    }))
   }
   render() {
     return (<div key={this.props.itemId} className="menu-item" id={`menu-item-${this.props.itemId}`}>
